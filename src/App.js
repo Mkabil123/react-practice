22de08e67bcd013c89cc9745fc97ca2d996f1a73
kabil <mkabil0210@gmail.com>
2022-07-04 12:49:26 +0530

    added validation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Mainpage from "./Components/Mainpage";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Mainpage from "./Components/Mainpage";
 import Homepage from "./Components/Homepage";
-import Desktop from "./Components/Desktop";
-import Mobilephone from "./Components/Mobilephone";
-import Laptop from "./Components/Laptop";
-import Bookpage from "./Components/Bookpage";
+
 function App() {
-  return (
-    <Router>
-      <Mainpage />
-      <Routes>
-        <Route path="/Home" element={<Homepage />}></Route>
-        <Route path="/Electronics/Desktop" element={<Desktop />}></Route>
-        <Route path="/Electronics/Laptop" element={<Laptop />}></Route>
-        <Route
-          path="/Electronics/Mobilephone"
-          element={<Mobilephone />}
-        ></Route>
-        <Route path="/Books" element={<Bookpage />}></Route>
-      </Routes>
-    </Router>
-  );
+  return <Homepage />;
 }
 
 export default App;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 import axios from "axios";
-import { Card, Form } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import Spinner from "react-bootstrap/Spinner";
 
 const Homepage = () => {
   const [data, getData] = useState([]);
@@ -10,119 +11,218 @@ const Homepage = () => {
     start: "",
     end: "",
   });
+  const [formError, setFormError] = useState({});
+  const [category, setCategory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const re = /^[0-9\b]+$/;
 
   const onChangeprice = (e) => {
-    setPrice({ ...price, [e.target.name]: e.target.value });
+    if (e.target.value === "" || re.test(e.target.value))
+      setPrice({ ...price, [e.target.name]: e.target.value });
   };
 
   const gettingdata = () => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
       getData(response.data);
       getFilter(response.data);
-      console.log(response.data);
+      setCategory(response.data);
     });
   };
 
   const filteritem = (event) => {
-    console.log(event, "yuygfuyfuyf");
     getData(
       filter.filter((datas) => {
-        // console.log(datas.dis_feetype_name);
-        return datas.category
+        let filtering = datas.title
           .toString()
           .toUpperCase()
           .includes(event.target.value.toUpperCase());
+        console.log(filtering);
+        if (filtering.length === 0) {
+          console.log("no dat f"); // <h1>no data dound</h1>;
+        } else return filtering;
       })
     );
   };
+
   const filtereditem = (e) => {
-    console.log(price, "yuygfuyfuyf");
     getData(
       filter.filter((datas) => {
+        setFormError(validate(price));
         let prices = datas.price;
-        return prices >= price.start && prices <= price.end;
+
+        return price.start && price.end
+          ? prices >= price.start && prices <= price.end
+          : datas.price;
       })
     );
   };
+
+  const categoryitem = (categories) => {
+    getData(
+      category.filter((datas) => {
+        if (categories.target.value === "All") {
+          gettingdata();
+        } else {
+          return datas.category
+            .toString()
+            .toUpperCase()
+            .includes(categories.target.value.toUpperCase());
+        }
+      })
+    );
+  };
+
   useEffect(() => {
     gettingdata();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
   }, []);
+
+  const validate = (price) => {
+    const errors = {};
+    if (!price.start) {
+      errors.start = "Starting value is required!";
+    }
+    if (!price.end) {
+      errors.end = "Ending value is required!";
+    }
+    return errors;
+  };
+
   return (
-    <div>
-      <div className="container">
-        <Card className="d-flex">
-          <h2>Filter By Price</h2>
-          <Form>
-            <input
-              style={{ width: "20%", height: "47%", marginLeft: "2%" }}
-              type="text"
-              name="start"
-              class="form-group"
-              placeholder="Enter starting price"
-              value={price.start}
-              onChange={onChangeprice}
-            ></input>
-            <input
-              style={{ width: "20%", height: "47%", margin: "2%" }}
-              type="text"
-              name="end"
-              class="form-group"
-              placeholder="Enter ending price"
-              value={price.end}
-              onChange={onChangeprice}
-            ></input>
-            <button
-              style={{ marginLeft: "2%" }}
-              type="button"
-              className="btn btn-primary"
-              onClick={filtereditem}
-            >
-              Filter
-            </button>
-            <h2>Filter By Category</h2>
-            <input
-              style={{ width: "25%", height: "47%", margin: "2%" }}
-              type="search"
-              class="form-control"
-              placeholder="type to search"
-              onChange={(event) => {
-                filteritem(event);
-              }}
-            ></input>
-          </Form>
-        </Card>
+    <div style={{ color: "steelblue", backgroundColor: "#FFFFFF" }}>
+      {loading ? (
+        <Spinner
+          animation="border"
+          variant="danger"
+          size="xxlg"
+          style={{
+            marginLeft: "47%",
+            marginTop: "10%",
+          }}
+        />
+      ) : (
+        <div className="container" style={{ Color: "bisque" }}>
+          <Card style={{ paddingLeft: "5%", backgroundColor: "whitesmoke" }}>
+            <h6>Filter By Price</h6>
+            <div className="row">
+              <div className="col-md-4">
+                <input
+                  type="number"
+                  name="start"
+                  className="form-control"
+                  placeholder="Enter starting price"
+                  value={price.start}
+                  onChange={onChangeprice}
+                ></input>
+                <p style={{ color: "red" }}>{formError.start}</p>
+              </div>
+              <div className="col-md-4">
+                <input
+                  type="number"
+                  name="end"
+                  className="form-control"
+                  placeholder="Enter ending price"
+                  value={price.end}
+                  onChange={onChangeprice}
+                ></input>
 
-        <div className="row">
-          {data.map((value) => {
-            return (
-              <div className="col -4">
-                <div
-                  className="card-body"
-                  style={{
-                    width: "18rem",
-                    height: "36rem",
-
-                    margin: "4%",
-                    backgroundColor: "lightgrey",
-                    borderBlockColor: "red",
+                <p style={{ color: "red" }}>{formError.end}</p>
+              </div>
+              <button
+                style={{ height: "40px", width: "70px" }}
+                type="button"
+                className="btn btn-primary col-md-3"
+                onClick={filtereditem}
+              >
+                Filter
+              </button>
+            </div>
+            <div className="row" style={{ marginBottom: "1%" }}>
+              <div className="col-md-4">
+                <h6>Filter By Title</h6>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search"
+                  onChange={(event) => {
+                    filteritem(event);
                   }}
-                >
-                  <img
-                    className="card-img-top"
-                    src={value.image}
-                    alt="Card image cap"
-                    width="75%"
-                    height="70%"
-                  />
-                  <h6 className="card-title">{`Id:${value.id}`}</h6>
-                  <h6 className="card-title">{`title:${value.title}`}</h6>
-                  <h6 className="card-title">{`Price:${value.price}`}</h6>
+                ></input>
+              </div>
+              <div className="col-md-2  "></div>
+
+              <div className="col-md-6  ">
+                <div>
+                  <select
+                    data-toggle="dropdown"
+                    className="custom-select"
+                    onChange={(categories) => {
+                      categoryitem(categories);
+                    }}
+                  >
+                    <option value="All">All</option>
+                    <option value="men's clothing">Mens Clothing</option>
+                    <option value="jewelery">Jewelery</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="women's clothing">Womens Clothing</option>
+                  </select>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          </Card>
+
+          <div className="row">
+            {data.map((value, index) => {
+              return (
+                <div className="col-md-4" key={index}>
+                  <div
+                    className="card "
+                    style={{
+                      height: "25rem",
+                      marginTop: "5%",
+
+                      backgroundColor: "#FAD94E",
+                      borderBlockColor: "black",
+                    }}
+                  >
+                    <div
+                      className="card-body-center"
+                      style={{ paddingTop: "25px " }}
+                    >
+                      <img
+                        className="card-img "
+                        src={value.image}
+                        alt="Card image cap"
+                        height="215px"
+                        style={{ width: "75%", paddingLeft: "25%" }}
+                      />
+
+                      <h6
+                        className="text-center"
+                        style={{
+                          color: "ThreeDHighlight",
+                          paddingTop: "20px",
+                        }}
+                      >
+                        {`$${value.price}`}
+                      </h6>
+                      <h6
+                        className=" text-center"
+                        style={{ paddingLeft: "33px", color: "green" }}
+                      >
+                        {value.title}
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
